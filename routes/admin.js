cde18e50834d42e9e0d11bbfe4b6368b2f8d1847
Adiiiikaushik <adityakaushik1801@gmail.com>
2024-09-25 12:54:00 +0530

    Created admin routes

---
+++ routes/admin.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const Router = express.Router
+
+const adminRouter = Router();
+
+adminRouter.post("/signup",(req,res)=>{
+    res.json({
+        message: "signup end point"
+    })
+})
+
+adminRouter.post("/signin",(req,res)=>{
+    res.json({
+        message: "signin end point"
+    })
+})
+
+adminRouter.post("/course",(req,res)=>{
+    res.json({
+        message: "make course end point"
+    })
+})
+
+adminRouter.put("/course",(req,res)=>{
+    res.json({
+        message: "put end point"
+    })
+})
+
+adminRouter.get("/course/bulk",(req,res)=>{
+    res.json({
+        message: "bulk end point"
+    })
+})
+
+module.exports = {
+    adminRouter,
+}
