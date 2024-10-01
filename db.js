c33c454d2b417af4d4cfe8638ab2e598f0b8a68e
Adiiiikaushik <adityakaushik1801@gmail.com>
2024-10-01 14:16:00 +0530

    added db

---
+++ db.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose")
+console.log("DB Connected")
+mongoose.connect("mongodb+srv://adityakaushik1801:9MgdDEDp6d6xnqbx@cluster0.yaubo.mongodb.net/coursera-clone")
+const ObjectId = mongoose.Types.ObjectId
+const Schema = mongoose.Schema
+
+
+const userSchema = new Schema({
+    email: {type: String, required: true},
+    password: String,
+    firstname: String,
+    lastName: String,
+
+});
+
+const adminSchema = new Schema({
+    email: {type: String, required: true},
+    password: String,
+    firstname: String,
+    lastName: String,
+
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+ 
+});
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
+
+});
+
+const userModel = mongoose.model("user",userSchema);
+const adminModel = mongoose.model("admin",adminSchema);
+const courseModel = mongoose.model("course",courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const Router = express.Router
+const { adminModel } = require("../db.js")
 
 const adminRouter = Router();
 
